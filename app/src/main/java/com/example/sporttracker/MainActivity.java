00c723ccae2a2b67f45075657e9db279d6ab9233
Clément Besnard <cle.besnard28@gmail.com>
2022-03-24 13:26:21 +0100

    hid bottom nav bar when start run

--- app/src/main/java/com/example/sporttracker/MainActivity.java
+++ app/src/main/java/com/example/sporttracker/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Paint;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.view.View;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
     BottomNavigationView bottomNavigationView;
     private RunningDbHelper runningDbHelper;
+    private HomeFragment homeFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +35,19 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         Toast.makeText(this, "Hello " + user.getFirstName(), Toast.LENGTH_SHORT).show();
 
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
 
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.activity);
+
+
     }
 
-    HomeFragment homeFragment = new HomeFragment();
-    ProfileFragment profileFragment = new ProfileFragment();
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -60,5 +66,9 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void startRunButton(View view) {
         homeFragment.startRun(view);
+        if (view.getId() == R.id.startRun)
+            bottomNavigationView.setVisibility(View.VISIBLE);
+        else
+            bottomNavigationView.setVisibility(View.GONE);
     }
 }
